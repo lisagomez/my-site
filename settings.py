459f67b736c78dffb30b96a9a820b4e4e77760e2
lisagomez <lisagomez967@gmail.com>
2020-02-23 09:38:45 -0600

    django

--- settings.py
+++
@@ -1,9 +0,0 @@
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-   'default': {
-       'ENGINE': 'django_mongodb_engine',
-       'NAME': 'my-site-db',
-   }
-}
