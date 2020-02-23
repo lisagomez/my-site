7ed39a65826d850d049e5a983ea2384993e240b3
lisagomez <lisagomez967@gmail.com>
2020-02-23 01:53:56 -0600

    first commit

---
+++ settings.py
@@ -0,0 +1,9 @@
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django_mongodb_engine',
+       'NAME': 'my-site-db',
+   }
+}
